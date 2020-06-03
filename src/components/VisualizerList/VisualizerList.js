3914ae2e6722a94c578bdfea4987edfb245f084b
Alan Henderson <mr.alan.henderson@gmail.com>
2020-06-03 09:13:09 -0500

    about to install moment js

--- src/components/VisualizerList/VisualizerList.js
+++ src/components/VisualizerList/VisualizerList.js
@@ -7,6 +7,7 @@ import VisualizerItem from '../VisualizerItem/VisualizerItem';
 
 
 
+
 export class VisualizerList extends PureComponent {
     componentDidMount() {
         this.props.dispatch({ type: "FETCH_HOURS", payload: this.props.clicked });
@@ -17,7 +18,7 @@ export class VisualizerList extends PureComponent {
     }
 
     clickHandler = () => {
-        console.log('this.props.clicked:', this.props.clicked, ", this props.userHistory:", [this.props.userHistory]);
+        console.log('In the Big Dumb Button! this.props.clicked:', this.props.clicked, ", this props.userHistory:", [this.props.userHistory], 'this.props.user.previous_year_carryover:', this.props.user.previous_year_carryover);
 
     }
 
@@ -29,10 +30,11 @@ export class VisualizerList extends PureComponent {
         //this is the array of objects in
         const calculatorArray = [];
         //this is the array to return
-        let sickTime = 0;
+        let sickTime = this.props.clicked.previous_year_carryover;
         let towardsAccrual = 0;
         let charged = 0;
-        //these are reference variables that record what was going on before each entry. I will want the sickTime to preload carryover when i'm at that point.
+        let accruedThisYear = 0;
+        //these are reference variables that record what was going on before each entry gets added.
         console.log('in payrollCalculator, arrayToCalculate=', arrayToCalculate);
         //start loop
         for (let i = 0; i < arrayToCalculate.length; i++) {
@@ -40,33 +42,48 @@ export class VisualizerList extends PureComponent {
                 date: arrayToCalculate[i].start,
                 sickAndSafe: 0,
                 accrual: 0,
-                chargeable: 0,
-                accruedThisYear: 0
+                chargeable: 0
             }
             //this is the object that needs to be filled in and added to the array that will feed into the graphs
             if (arrayToCalculate[i].payroll_code === 1 && arrayToCalculate[i].mpls ) {
                 console.log('recording regular hours');
                 towardsAccrual += arrayToCalculate[i].hours;
-                if (towardsAccrual >= 30 && arrayItem.sickAndSafe < 80 ) {
-                    towardsAccrual = towardsAccrual - 30;
-                    sickTime += 1;
+                if (accruedThisYear >= 48){
+                    towardsAccrual = 0;
                     arrayItem.sickAndSafe = sickTime;
                     arrayItem.accrual = towardsAccrual;
+                    arrayItem.chargeable = charged
+                } else if (towardsAccrual >= 30 && arrayItem.sickAndSafe < 80 ) {
+                    towardsAccrual = towardsAccrual - 30;
+                    sickTime += 1;
+                    accruedThisYear += 1;
+                    arrayItem.sickAndSafe = sickTime;
+                    arrayItem.accrual = towardsAccrual;
+                    arrayItem.chargeable = charged
+                } else if (towardsAccrual >= 30 && arrayItem.sickAndSafe >= 80 ){
+                    towardsAccrual = 0;
+                    arrayItem.sickAndSafe = sickTime;
+                    arrayItem.accrual = towardsAccrual;
+                    arrayItem.chargeable = charged
+                } else if (sickTime >= 80 ){
+                    towardsAccrual = 0;
+                    arrayItem.sickAndSafe = 80;
+                    arrayItem.accrual = 0;
                     arrayItem.chargeable = charged
                 } else {
                     arrayItem.sickAndSafe = sickTime;
                     arrayItem.accrual = towardsAccrual;
                     arrayItem.chargeable = charged
-                };
+                };//end logic for Regular Hours in Minneapolis
             } else {
                 arrayItem.sickAndSafe = sickTime;
                 arrayItem.accrual = towardsAccrual;
                 arrayItem.chargeable = charged
-            }
-            ;//end process Regular Hours
+            }//end logic for work down outside Minneapolis
+            ;//end the process for Regular Hours
             if (arrayToCalculate[i].payroll_code === 2) {
                 console.log('recording vacation');
-                //until a future build where I am tracking vacation this doesn't need to do anything other than mark the passage of time with values unchanged
+                //until a future build where I want to track vacation this doesn't need to do anything other than mark the passage of time with values unchanged
                 arrayItem.sickAndSafe = sickTime;
                 arrayItem.accrual = towardsAccrual;
                 arrayItem.chargeable = charged
@@ -225,82 +242,10 @@ export class VisualizerList extends PureComponent {
 
 const mapStateToProps = state => ({
     clicked: state.clicked,
-    userHistory: state.userHistory
+    userHistory: state.userHistory,
+    user: state.user
 });
 
 export default connect(mapStateToProps)(VisualizerList);
 
 
-//The demo code from reChart
-
-// import React, { PureComponent } from 'react';
-// import {
-//     AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
-// } from 'recharts';
-
-// const data = [
-//     {
-//         name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-//     },
-//     {
-//         name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-//     },
-//     {
-//         name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-//     },
-//     {
-//         name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-//     },
-//     {
-//         name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-//     },
-//     {
-//         name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-//     },
-//     {
-//         name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-//     },
-// ];
-
-// export default class Example extends PureComponent {
-//     static jsfiddleUrl = 'https://jsfiddle.net/alidingling/2vyv694u/';
-
-//     render() {
-//         return (
-//             <div>
-//                 <h4>A demo of synchronized AreaCharts</h4>
-//                 <AreaChart
-//                     width={500}
-//                     height={200}
-//                     data={data}
-//                     syncId="anyId"
-//                     margin={{
-//                         top: 10, right: 30, left: 0, bottom: 0,
-//                     }}
-//                 >
-//                     <CartesianGrid strokeDasharray="3 3" />
-//                     <XAxis dataKey="name" />
-//                     <YAxis />
-//                     <Tooltip />
-//                     <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-//                 </AreaChart>
-//                 <p>Maybe some other content</p>
-//                 <AreaChart
-//                     width={500}
-//                     height={200}
-//                     data={data}
-//                     syncId="anyId"
-//                     margin={{
-//                         top: 10, right: 30, left: 0, bottom: 0,
-//                     }}
-//                 >
-//                     <CartesianGrid strokeDasharray="3 3" />
-//                     <XAxis dataKey="name" />
-//                     <YAxis />
-//                     <Tooltip />
-//                     <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
-//                 </AreaChart>
-//             </div>
-//         );
-//     }
-// }
